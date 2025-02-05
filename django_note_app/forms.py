e617675fa6dbb70caefa7dd60db22f72917197ca
Andrii M <andriymiroshnik@gmail.com>
2025-02-05 20:44:07 +0200

    bootstrap styles

--- django_note_app/forms.py
+++ django_note_app/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django_note_app.models import Task
 
-
-
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'category', 'assignee', 'priority', 'deadline', 'status']
+        fields = ['name', 'assignee', 'priority', 'deadline', 'status']
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Enter task name'}),
-            'deadline': forms.DateInput(attrs={'type': 'date'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'assignee': forms.TextInput(attrs={'class': 'form-control'}),
+            'priority': forms.Select(attrs={'class': 'form-select'}),
+            'deadline': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
         }
--- django_note_app/views.py
+++ django_note_app/views.py
@@ -1,27 +1,23 @@
-from django.shortcuts import redirect
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect, render
+from django.views.generic import UpdateView, DeleteView
 from django.urls import reverse_lazy
-from django_note_app.models import Task
-from django_note_app.forms import TaskForm
+from django.views import View
+from .models import Task
+from .forms import TaskForm
 
-class TaskListView(ListView):
-    model = Task
-    template_name = "note_app_templates/tasks.html"
-    context_object_name = 'tasks'
+class TaskListView(View):
+    def get(self, request):
+        tasks = Task.objects.all()  # Fetch tasks from the database
+        form = TaskForm()           # Load the form
+        return render(request, 'note_app_templates/tasks.html', {'tasks': tasks, 'form': form})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = TaskForm()
-        return context
-
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('show_all_tasks')
-        return self.get(request, form=form)
+        tasks = Task.objects.all()
+        return render(request, 'note_app_templates/tasks.html', {'tasks': tasks, 'form': form})
 
-# View to Edit Tasks
 class TaskUpdateView(UpdateView):
     model = Task
     form_class = TaskForm
